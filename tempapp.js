75fea688ab00333e46fda6e24a88ea762779b648
Andrew <kirkmanandrew@aol.com>
2023-10-22 21:43:21 -0400

    Setting up VM

--- tempapp.js
+++ tempapp.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+

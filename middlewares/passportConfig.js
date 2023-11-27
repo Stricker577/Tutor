f1d70efe6eded58a9c6b4d458fce3fbffb729e1c
Andrew <kirkmanandrew@aol.com>
2023-11-27 18:03:40 -0500

    testing

--- middlewares/passportConfig.js
+++ middlewares/passportConfig.js
@@ -2,21 +2,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose')
 const User = require('../models/user')
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // Here, you would find or create a user in your database
-    // For demonstration, we just return the Google profile
-    return cb(null, profile);
-  }
-));
-
-
 module.exports = function (passport) {
   passport.use(
     new GoogleStrategy(

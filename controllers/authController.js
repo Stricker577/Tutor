0e195cbe258db6d539ce403321d1560b7d3c0f7c
Andrew <kirkmanandrew@aol.com>
2023-11-27 17:55:15 -0500

    testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,8 @@ app.get('/auth/google/callback',
     }
 );
 
+app.listen(3000, () => console.log('Server started on port 3000'));
+
 exports.googleCallback = (req, res, next) => {
   passport.authenticate('google', (err, user, info) => {
     if (err) {

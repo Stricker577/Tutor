7c35c103ee391b0185b07d3cb22310c2298dd7f7
Andrew <kirkmanandrew@aol.com>
2023-11-27 20:18:23 -0500

    testing

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  googleId: {type: String,required: true,},
-  displayName: {type: String,required: true,},
-  firstName: {type: String,required: true,},
-  lastName: {type: String,required: true,},
-  image: {type: String,},
-  email:{type:String,required: true,},
+  googleId: {type: String},
+  displayName: {type: String},
+  firstName: {type: String},
+  lastName: {type: String},
+  image: {type: String},
+  email:{type:String},
   createdAt: {type: Date,default: Date.now,}
 });
 

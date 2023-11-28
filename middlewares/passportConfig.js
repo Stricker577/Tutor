64c701273c8e31c2297c3aff6daab9cbb2c1ba31
Andrew <kirkmanandrew@aol.com>
2023-11-27 19:57:26 -0500

    testing

--- middlewares/passportConfig.js
+++ middlewares/passportConfig.js
@@ -19,8 +19,8 @@ module.exports = function (passport) {
   passport.use(
     new GoogleStrategy({
         // options for google strategy
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db

4d0d105aa867197276a678921fe55da56d93fb97
Andrew <kirkmanandrew@aol.com>
2023-11-27 20:26:32 -0500

    Users are being created!

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -9,7 +9,7 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.render('profile', { user: req.user });
+    res.render('user/profile', { user: req.user });
 });
 
 module.exports = router;
